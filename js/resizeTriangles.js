776e4c56ec67f05c198d6ff03ed734ef4ceda900
Juan Pablo Díaz Correa <juanipis@gmail.com>
2021-03-30 15:47:31 -0500

    organización de resizeTriangle.js

--- js/resizeTriangles.js
+++ js/resizeTriangles.js
@@ -1,19 +1,18 @@
 
+/* Cuando se carga el doom */
 document.addEventListener("DOMContentLoaded", function () { 
   var red = document.querySelector('#escaleno');
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 
 
-function changeAnimationTime() {
-  red.style.setProperty('--width-a', getRandomArbitrary(0, 50) +'%');
-  red.style.setProperty('--width-b', getRandomArbitrary(0, 40) +'%');
-  red.style.setProperty('--width-c', getRandomArbitrary(90, 100) +'%');
-
-}
-  
-
-setInterval(changeAnimationTime, 4000);
-});
+  function changeAnimationTime() {
+    red.style.setProperty('--width-a', getRandomArbitrary(0, 50) +'%');
+    red.style.setProperty('--width-b', getRandomArbitrary(0, 40) +'%');
+    red.style.setProperty('--width-c', getRandomArbitrary(90, 100) +'%');
+  }
+  setInterval(changeAnimationTime, 4000);
+  }
+);
