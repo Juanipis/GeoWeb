88e64722f98b2d4e1102ce38da738459f2acde48
Juan Pablo Diaz Correa <juanipis@gmail.com>
2021-04-07 22:18:41 -0500

    inicio de triangulo obtusangulo, animacion en javascript

--- js/resizeTriangles.js
+++ js/resizeTriangles.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () { 
   const $escaleno = document.querySelector('#escaleno');
   
+  
 
   function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
@@ -17,5 +18,17 @@ document.addEventListener("DOMContentLoaded", function () {
     l
   }
   setInterval(changeAnimationTime, 6000);
+
+  var obtusangulo = document.getElementById('obtusangulo');
+  var hola = obtusangulo.getAttribute('points');
+  var valorXObtuso = 90.1;
+  function animate_obtusangulo(){
+    console.log("hola mundo");
+    valorXObtuso = valorXObtuso+0.1;
+    obtusangulo.setAttribute('points', '100 '+ valorXObtuso +', 150 150, 300 150')
+  }
+  
+  setInterval(animate_obtusangulo, 20)
+  
   }
 );
