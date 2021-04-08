aab5f44e2f01f93a82b16a4386b675abb00274cb
Juan Pablo Diaz Correa <juanipis@gmail.com>
2021-04-07 19:00:48 -0500

    angulo agudo final gracias a @ditopixels

---
+++ js/angles.js
@@ -0,0 +1,23 @@
+/*
+document.addEventListener("DOMContentLoaded", function () { 
+    var angulo = document.getElementById('agudo');
+    var value = 0;
+    var condicion = true
+
+    function anguloAgudo(){
+        angulo.setAttribute('transform', 'rotate(-'+valores() + ' 50 50)');
+        
+    }
+    function valores() {
+        if (value == 0) {
+            
+        }
+        value = value + 0.1;
+        return value;
+    }
+
+
+  setInterval(anguloAgudo, 10);
+  }
+);
+*/
